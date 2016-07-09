b5313d84ca520028bcc5f5d1cf5cb502f797f874
Cameron Perrin <perrin.cameron@gmail.com>
2016-07-09 17:50:54 +0100

    stats are all working

--- main.js
+++ main.js
@@ -5,9 +5,12 @@ var game = {
   enemyAtk: 10,
   enemyDefence: 5,
   enemyLife: 50,
+  enemyMaxLife: 50,
   playerMaxLife: 50,
   clickability: true,
   barLength: 220,
+  enemyBarLength: 220,
+  getsToDecide: true,
   decision: Math.random(),
   attack: function(){
 //lowers your defense for attacking
@@ -29,7 +32,7 @@ var game = {
     switch(game.playerAtk<=game.enemyDefence) {
       case true:
       game.enemyDefense = game.enemyDefense - Math.floor(.5*game.playerAtk)
-      console.log("you cant do enough damage!");
+      $(textInfo).html("You can't do enough damage!");
       console.log("enemy Life: "+game.enemyLife);
       break;
 
@@ -48,7 +51,14 @@ var game = {
     switch(game.enemyLife <= 0){
       case true:
       game.enemyLife = 0
-      console.log("YOU WIN")
+      game.getsToDecide = false
+      $(textInfo).html("Congratulations, YOU WIN!");
+      setTimeout(function(){
+        $("#enemy").html("<img id='enemyAvatar' src='images/enemy-dead.png'>");
+        $("#enemyAvatar").css("height", "100px");
+        $("#enemyAvatar").css("width", "200px");
+        $("#enemyAvatar").css("bottom", "175px");
+      }, 2000)
       game.playerLife = game.playerMaxLife
       break;
 
@@ -84,6 +94,68 @@ var game = {
   enemyAttack: function(){
 //lowers enemy defense for attacking
     game.enemyDefence = game.enemyDefence - (Math.ceil(Math.random()*3));
+//plays animation
+    $("#enemy").html("<img id='enemyAvatar' src='images/enemy-move-out.gif'>");
+    $("#enemy").css("right", "50px");
+    $("#enemyAvatar").css("height", "200px");
+
+    setTimeout(function(){
+      $("#enemy").css("right", "1000px");
+      $("#enemy").html("<img id='enemyAvatar' src='images/enemy-move-in.gif'>");
+    }, 300);
+
+    setTimeout(function(){
+      $("#enemy").html("<img id='enemyAvatar' src='images/enemy-attack1.png'>");
+    }, 1000);
+
+    setTimeout(function(){
+      $("#enemy").html("<img id='enemyAvatar' src='images/enemy-attack2.png'>");
+      $("#enemyAvatar").css("width", "220px");
+      $("#player").html("<img id='avatar' src='images/player-hurt.png'>");
+      $("#avatar").css("width", "150px");
+    }, 1300);
+
+    setTimeout(function(){
+      $("#enemy").html("<img id='enemyAvatar' src='images/enemy-move-in.gif'>");
+      $("#enemy").css("right", "1150px");
+      $("#enemy").css("bottom", "50px");
+      $("#enemy").css("z-index", "-1");
+    }, 1600);
+
+    setTimeout(function(){
+      $("#enemy").html("<img id='enemyAvatar' src='images/enemy-attack3.png'>");
+      $("#enemy").css("bottom", "60px");
+      $("#enemy").css("z-index", "-1");
+      $("#enemyAvatar").css("width", "130px");
+    }, 2200);    
+
+    setTimeout(function(){
+      $("#enemy").html("<img id='enemyAvatar' src='images/enemy-attack4.png'>");
+      $("#player").html("<img id='avatar' src='images/player-hurt2.png'>");
+      $("#enemyAvatar").css("width", "250px");
+      $("#enemy").css("right", "1080px");
+    }, 2500);  
+
+    setTimeout(function(){
+      $("#enemy").html("<img id='enemyAvatar' src='images/enemy-move-out.gif'>");
+      $("#enemy").css("right", "1150px");
+      $("#enemy").css("bottom", "50px");
+      $("#enemy").css("z-index", "-1");
+    }, 2800);
+
+    setTimeout(function(){
+      $("#enemy").css("right", "0px");
+      $("#enemy").css("bottom", "0px");
+      $("#enemy").html("<img id='enemyAvatar' src='images/enemy-move-in.gif'>");
+      $("#player").html("<img id='avatar' src='images/idle.gif'>");
+    }, 3100);
+
+    setTimeout(function(){
+      $("#enemy").html("<img id='enemyAvatar' src='images/enemy-idle.gif'>");
+      $("#enemyAttack").html("Attack: "+game.enemyAtk)
+
+    }, 3400);
+
 //doesn't allow enemy defense to go below 0
     switch(game.enemyDefence<0) {
       case true:
@@ -100,7 +172,7 @@ var game = {
 //doesn't allow you to do damage if your attack is lower than enemy defense
     switch(game.enemyAtk<=game.playerDefence) {
       case true:
-      console.log("It cant do enough damage!");
+      $(textInfo).html("The enemy cant do enough damage!");
       game.playerDefence = game.playerDefence - Math.floor(.4*game.enemyAtk)
       break;
 
@@ -110,7 +182,10 @@ var game = {
       console.log("damage dealt to me: "+(game.enemyAtk-game.playerDefence))
       break;
     };
-
+    setTimeout(function(){
+    //updates player health
+        $("#playerHealth").html("Health: "+game.playerLife+"/"+game.playerMaxLife);
+    }, 3100)
 //increases enemy attack for attacking
     game.enemyAtk = game.enemyAtk + (Math.ceil(Math.random()*(4)+1));
     console.log("enemy's new attack: "+game.enemyAtk);
@@ -119,15 +194,26 @@ var game = {
     switch(game.playerLife <= 0){
       case true:
       game.playerLife = 0
-      console.log("GAME OVER")
+      $(textInfo).html("GAME OVER");
+      setTimeout(function(){
+        $("#player").html("<img id='avatar' src='images/enemy-dead.png'>");
+        $("#avatar").css("height", "100px");
+        $("#avatar").css("width", "200px");
+        $("#avatar").css("bottom", "175px");
+      }, 3350)
       break;
 
       case false:
       console.log("your Life: "+game.playerLife)
       break;
     };
+    //updates playes health
+    game.lifeBar();
+    $("#playerHealthBar").css("width", game.barLength);
   },
   enemyDefend: function(){
+//animation for defense
+  $("#enemy").html("<img id='enemyAvatar' src='images/enemy-defend.gif'>");
 
 //Increases defense
     game.enemyDefence = game.enemyDefence + (Math.ceil(Math.random()*(5)+2));
@@ -148,7 +234,8 @@ var game = {
 
 
     };
-
+    //updates defense
+    $("#enemyDefence").html("Defence: "+game.enemyDefence);
   },
   //lets the enemy decide to attack or defend
   decide: function(){
@@ -175,6 +262,10 @@ var game = {
 
   lifeBar: function(){
   game.barLength = ((game.playerLife / game.playerMaxLife) * 220);
+  },
+
+  enemyLifeBar: function(){
+    game.enemyBarLength = ((game.enemyLife / game.enemyMaxLife) * 220);
   }
 
 };
@@ -184,14 +275,20 @@ $("#attack").on("mouseup", function(){
   if(game.clickability === true){
     game.clickability = false;
     game.attack();
-
-    console.log("----------------------")
-
-    setTimeout(game.decide, 2300);
+    //updates enemy health
+    game.enemyLifeBar();
+    $("#enemyHealthBar").css("width", game.enemyBarLength);
+    $("#enemyHealthNumber").html("Health: "+game.enemyLife+"/50")
 
     setTimeout(function(){
-      game.lifeBar();
-      $("#playerHealthBar").css("width", game.barLength);
+      game.clickability = true;
+    }, 5800);
+
+    console.log("----------------------")
+    if(game.getsToDecide === true){
+    setTimeout(game.decide, 2300);
+    };
+    setTimeout(function(){
       $("#playerHealth").html("Health: "+game.playerLife+"/"+game.playerMaxLife);
       $("#playerDefence").html("Defence: "+game.playerDefence);
       $("#playerAttack").html("Attack: "+game.playerAtk)
@@ -242,10 +339,11 @@ $("#defend").on("mouseup", function(){
     game.clickability = false;
   game.defend();
   console.log("----------------------")
+  if(game.getsToDecide === true){
   setTimeout(game.decide, 1000);
+  };
+  $("#playerDefence").html("Defence: "+game.playerDefence);
   setTimeout(function(){
-    game.lifeBar();
-    $("#playerHealthBar").css("width", game.barLength);
     $("#playerHealth").html("Health: "+game.playerLife+"/"+game.playerMaxLife);
     $("#playerDefence").html("defense: "+game.playerDefence);
     game.clickability = true;
